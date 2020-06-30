563d8737586c8c15b5f7e0669930197905ee2f69
TranslucentFoxHuman <translucentfoxhuman@outlook.jp>
2020-06-30 21:51:56 +0900

    READMEの完成

--- src/tlclipedit.c
+++ src/tlclipedit.c
@@ -1,7 +1,7 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
 /*
  * tlclipedit.c
- * Copyright (C) 2020 TranslucentFoxHuman
+ * 
  * 
  * TLClipEdit is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
